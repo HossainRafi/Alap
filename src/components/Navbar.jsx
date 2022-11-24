a9d41a8b605feed254e28ef42d4fca25973e3731
HossainRafi <hossainrafi71@gmail.com>
2022-11-24 20:24:29 +0600

    console user

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const style = {
   nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
@@ -6,6 +8,8 @@ const style = {
 };
 
 const Navbar = () => {
+  const [user] = useAuthState(auth);
+  console.log(user);
   return (
     <div className={style.nav}>
       <h1 className={style.heading}>TidBit</h1>
