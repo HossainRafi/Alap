b2ccc683dd93037b367b559da40ccbffbc02493c
HossainRafi <hossainrafi71@gmail.com>
2022-11-24 19:46:12 +0600

    navbar component create

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
-  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-200 mt-10 shadow-xl border relative`,
 };
 
 function App() {
   return (
     <div className={style.appContainer}>
-      <section className="{style.sectionContainer}">
-        
+      <section className={style.sectionContainer}>
+
       </section>
     </div>
   );
