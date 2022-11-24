31812d1fe6899d0ab8ae59f45116db6ebca80b4f
HossainRafi <hossainrafi71@gmail.com>
2022-11-24 19:37:26 +0600

    container style set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+const style = {
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+};
+
 function App() {
   return (
-    <div>
+    <div className="">
       <h1>
         This Project Contains The Basic Setup Of A React App With Tailwind CSS
       </h1>
