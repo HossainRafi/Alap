797be8bacc075eef36cf8ed7e02b2e5de503ca49
HossainRafi <hossainrafi71@gmail.com>
2022-11-25 21:02:18 +0600

    send msg and receive msg design

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { auth } from "../firebase";
 
 const style = {
   message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
@@ -8,9 +9,14 @@ const style = {
 };
 
 const Message = ({ message }) => {
+  const messageClass =
+    message.uid === auth.currentUser.uid
+      ? `${style.sent}`
+      : `${style.received}`;
+
   return (
     <div>
-      <div className={style.message}>
+      <div className={`${style.message} ${messageClass}`}>
         <p className={style.name}>{message.name}</p>
         <p>{message.text}</p>
       </div>
