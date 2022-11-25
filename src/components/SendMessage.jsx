67c103b0f5a91097e2be55cdd43f57fa73c0aa71
HossainRafi <hossainrafi71@gmail.com>
2022-11-25 20:51:13 +0600

    making send message functional

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { auth, db } from "../firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 const style = {
   form: `h-14 w-full max-w-[728px]  flex text-xl absolute bottom-0`,
@@ -6,11 +8,28 @@ const style = {
   button: `w-[20%] bg-green-500`,
 };
 
-const SendMessage = () => {
+const SendMessage = ({ scroll }) => {
   const [input, setInput] = useState("");
 
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    if (input === "") {
+      alert("Please enter a valid message");
+      return;
+    }
+    const { uid, displayName } = auth.currentUser;
+    await addDoc(collection(db, "messages"), {
+      text: input,
+      name: displayName,
+      uid,
+      timestamp: serverTimestamp(),
+    });
+    setInput("");
+    scroll.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <form className={style.form}>
+    <form onSubmit={sendMessage} className={style.form}>
       <input
         value={input}
         onChange={(e) => setInput(e.target.value)}
