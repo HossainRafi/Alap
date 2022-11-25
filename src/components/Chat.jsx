351a27b42bcc0fc2adce9d09cae3d03385f726eb
HossainRafi <hossainrafi71@gmail.com>
2022-11-25 20:07:28 +0600

    state declared for message import

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,27 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Message from "./Message";
+import { db } from "../firebase";
+import { query, collection, orderBy, onSnapshot } from "firebase/firestore";
 
 const style = {
   main: `flex flex-col p-[10px]`,
 };
 
 const Chat = () => {
-  const scroll = useRef();
+    const [messages, setMessages] = useState([]);
+    const scroll = useRef();
+    
+    useEffect(() => {
+      const q = query(collection(db, "messages"), orderBy("timestamp"));
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        let messages = [];
+        querySnapshot.forEach((doc) => {
+          messages.push({ ...doc.data(), id: doc.id });
+        });
+        setMessages(messages);
+      });
+      return () => unsubscribe();
+    }, []);
   return (
     <>
       <main className={style.main}>
