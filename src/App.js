e899ad9aa096417b0e4903069ce47df7b76f4eba
HossainRafi <hossainrafi71@gmail.com>
2022-11-25 19:56:02 +0600

    message shown in ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 
 import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Chat from "./components/Chat";
 
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
@@ -15,6 +16,7 @@ function App() {
     <div className={style.appContainer}>
       <section className={style.sectionContainer}>
         <Navbar />
+        <Chat />
       </section>
     </div>
   );
