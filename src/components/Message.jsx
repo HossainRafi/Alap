662b657621c17d11f385ec4cfba6718aab52b194
HossainRafi <hossainrafi71@gmail.com>
2022-11-25 19:53:23 +0600

    name and message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 const style = {
   message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
-
+  name: `absolute mt-[-4rem] text-gray-600 text-xs`,
 };
 
 const Message = () => {
   return (
     <div>
-      <div className={style.message}></div>
+          <div className={style.message}>
+              <p className={style.name}></p>
+              <p>Hello my friend........</p>
+      </div>
     </div>
   );
 };
