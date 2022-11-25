56cb149a3f6525002a8a966f20f3f45876a16d7f
HossainRafi <hossainrafi71@gmail.com>
2022-11-25 19:54:36 +0600

    design for send and receive messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,6 +3,8 @@ import React from "react";
 const style = {
   message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
   name: `absolute mt-[-4rem] text-gray-600 text-xs`,
+  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
+  received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
 };
 
 const Message = () => {
