82095b3f40e953c55c6d93526dd497ccfc339978
HossainRafi <hossainrafi71@gmail.com>
2022-11-25 23:45:51 +0600

    edit code

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { auth } from "../firebase";
 
 const style = {
-  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
+  message: `flex items-center shadow-xl m-4 py-1 px-3 rounded-tl-full rounded-tr-full`,
   name: `absolute mt-[-4rem] text-gray-600 text-xs`,
   sent: `bg-[#27ae60] text-white flex-row-reverse text-end float-right rounded-bl-full`,
   received: `bg-[#16a085] text-black float-left rounded-br-full`,
