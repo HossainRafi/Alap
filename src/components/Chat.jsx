b088ac41cc5825834f178534ab33eef3d80f1f7c
HossainRafi <hossainrafi71@gmail.com>
2022-11-25 19:43:59 +0600

    chat component

---
+++ src/components/Chat.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const style = {
+  main: `flex flex-col p-[10px]`,
+};
+
+const Chat = () => {
+  return (
+    <div className={style.main}>Chat</div>
+  )
+}
+
+export default Chat
