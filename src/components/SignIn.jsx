146ffaa05df7d01f7787361591ce52c1f47c33ab
HossainRafi <hossainrafi71@gmail.com>
2022-11-25 23:21:04 +0600

    signin btn design

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { auth } from "../firebase";
 
 const style = {
-  button: `bg-gray-200 px-4 py-2 hover:bg-gray-100 font-bold rounded-md`,
+  button: `bg-gray-200 px-4 py-2 hover:bg-gray-300 font-bold rounded-md`,
 };
 
 const googleSignIn = () => {
