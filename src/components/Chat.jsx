bd19767c3d43515a7ffe5215172d79c427d23886
HossainRafi <hossainrafi71@gmail.com>
2022-11-25 21:26:05 +0600

    chat done

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,28 +1,29 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Message from "./Message";
+import SendMessage from "./SendMessage";
 import { db } from "../firebase";
 import { query, collection, orderBy, onSnapshot } from "firebase/firestore";
-import SendMessage from "./SendMessage";
 
 const style = {
   main: `flex flex-col p-[10px]`,
 };
 
 const Chat = () => {
-    const [messages, setMessages] = useState([]);
-    const scroll = useRef();
-    
-    useEffect(() => {
-      const q = query(collection(db, "messages"), orderBy("timestamp"));
-      const unsubscribe = onSnapshot(q, (querySnapshot) => {
-        let messages = [];
-        querySnapshot.forEach((doc) => {
-          messages.push({ ...doc.data(), id: doc.id });
-        });
-        setMessages(messages);
+  const [messages, setMessages] = useState([]);
+  const scroll = useRef();
+
+  useEffect(() => {
+    const q = query(collection(db, "messages"), orderBy("timestamp"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let messages = [];
+      querySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
       });
-      return () => unsubscribe();
-    }, []);
+      setMessages(messages);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <main className={style.main}>
