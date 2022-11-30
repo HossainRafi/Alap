25b23de3546c0b2e1f9ba38cbefaa8206d3e7a8c
HossainRafi <hossainrafi71@gmail.com>
2022-11-30 23:50:43 +0600

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ import LogOut from "./LogOut";
 
 const style = {
   nav: `bg-gray-800 h-10 flex justify-between items-center py-4 pl-4`,
-  heading: `text-white text-3xl`,
+  heading: `text-white text-3xl font-serif`,
 };
 
 const Navbar = () => {
   const [user] = useAuthState(auth);
   return (
     <div className={style.nav}>
-      <h1 className={style.heading}>TidBit</h1>
+      <h1 className={style.heading}>AlaP</h1>
       {user ? <LogOut /> : <SignIn />}
     </div>
   );
